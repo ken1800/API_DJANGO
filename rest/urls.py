e46f4e49be3762c370235a4126d8faa1cf54bc17
kenneth irungu <kennethirungu1800@gmail.com>
2020-02-05 17:20:30 +0300

    complited the user django rest api

--- rest/urls.py
+++ rest/urls.py
@@ -1,10 +1,13 @@
 
-from .views import HelloApiView,HelloViewSet
+from .views import HelloApiView,HelloViewSet,UserProfileViewSet,UserLoginApiView,UserProfileFeedViewSet
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('hello-viewset',HelloViewSet, basename ='hello-viewset')
+router.register('hello-viewset/',HelloViewSet, basename ='hello-viewset')
+router.register('profiles',UserProfileViewSet)
+router.register('feed',UserProfileFeedViewSet)
+
 
 
 
@@ -12,6 +15,7 @@ router.register('hello-viewset',HelloViewSet, basename ='hello-viewset')
 
 urlpatterns = [
     path('proff/',HelloApiView.as_view()),
+    path('login/',UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
 
